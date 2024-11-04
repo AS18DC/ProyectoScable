a8f28d520e924d12d05fd45a22714bc98a068997
Ali.D <alidahoukc@gmail.com>
2024-11-04 18:40:14 -0400

    prueba de git

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,10 +1,12 @@
 public class Jugador {
     private String correo;
     private String alias;
+    private int victorias;
 
     public Jugador(String correo, String alias) {
         this.correo = correo;
         this.alias = alias;
+        this.victorias = 0;
     }
 
     public String getCorreo() {
