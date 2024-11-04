49e8e678db7a10428c414da4795cc720544f37f3
Ali.D <alidahoukc@gmail.com>
2024-11-04 18:27:38 -0400

    creacion jugadores, RegistroJugadores

---
+++ src/Jugador.java
@@ -0,0 +1,17 @@
+public class Jugador {
+    private String correo;
+    private String alias;
+
+    public Jugador(String correo, String alias) {
+        this.correo = correo;
+        this.alias = alias;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+}
---
+++ src/RegistroJugadores.java
@@ -0,0 +1,8 @@
+public class RegistroJugadores extends Jugador{
+
+    public RegistroJugadores(String correo, String alias) {
+        super(correo, alias);
+    }
+
+    
+}
